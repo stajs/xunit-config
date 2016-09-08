214af5720102a190b0c91e1ba1ac79bcaec2fbd9
stajs <shannonsnz@gmail.com>
2016-09-08 22:06:28 +1200

    2.2.0-preview3-build1030

---
+++ Xunit.Config/src/Xunit.Config.Net461/ConfigurationManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace Xunit.Config.Net461
+{
+	public class ConfigurationManagerTests
+	{
+		[Fact]
+		public void CanReadAppConfig()
+		{
+			var appSettings = ConfigurationManager.AppSettings;
+			Console.WriteLine("appSettings: " + (appSettings == null ? "null" : "not null"));
+			Console.WriteLine("appSettings[\"Foo\"]: " + appSettings["Foo"]);
+			Assert.NotNull(ConfigurationManager.AppSettings);
+			Assert.Equal("Bar", appSettings["Foo"]);
+		}
+	}
+}
